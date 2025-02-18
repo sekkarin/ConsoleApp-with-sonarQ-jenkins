a2b822b2598fee3081ef7a3a0c07963b9d59e4bc
sekkarin.s <sekkarin.s@gmail.com>
2025-02-18 11:06:37 +0700

    fix:sonar-pro.proper..

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab67cfa6a86b4294266438c096a9b663c7f0820d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cfaef56d505a1455a6acc7d4d1905e7675a15e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
